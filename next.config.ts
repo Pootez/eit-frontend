ab52efda303e12c7c818b6292e0b1d23e1b3f26c
Sondre Espeland <sondrenoe@gmail.com>
2026-04-15 14:19:43 +0200

    Attempt github pages

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,20 @@
 import type { NextConfig } from "next"
 
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = process.env.REPO_NAME;
+
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
-  assetPrefix: "./",
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : './',
 }
 
 export default nextConfig
+
+module.exports = {
+  output: 'export',
+};
