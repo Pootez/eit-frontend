3074cc1b1b5df049adcb28762f48585c2b46e37d
Sondre Espeland <sondrenoe@gmail.com>
2026-04-15 14:36:32 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,12 @@
 import type { NextConfig } from "next"
 
-const isProd = process.env.NODE_ENV === 'production';
-const repoName = process.env.REPO_NAME;
-
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
-  basePath: isProd ? `/${repoName}` : '',
-  assetPrefix: isProd ? `/${repoName}/` : './',
+  assetPrefix: "./",
 }
 
 export default nextConfig
